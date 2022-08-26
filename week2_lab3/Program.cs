f19b36f0b350dcecbc7a10ad87c6be60d12784bf
Eddie Osmanbasic <eddieosmanbasic@gmail.com>
2022-08-26 00:30:27 -0400

    adding files

---
+++ week2_lab3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+
+Console.Write("Please enter a word you would like to reverse: ");
+string entry = Console.ReadLine();
+string reversedString = ReverseString(entry);
+Console.WriteLine($"Your word in reverse is: {reversedString}");
+
+static string ReverseString(string entry)
+{
+    Stack<char> stackedString = new Stack<char>();
+    for (int i = 0; i < entry.Count(); i++)
+    {
+        stackedString.Push(entry[i]);
+        
+    }
+    string reversed = String.Join("", stackedString);
+    return reversed;
+}
